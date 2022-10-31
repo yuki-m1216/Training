2ec1f779b2271119df7e902dce3d52031b996cb5
Yuki Mitsuyama <55880586+y-mitsuyama1216@users.noreply.github.com>
2022-11-01 01:45:16 +0900

    add lb,target gorup

--- Resources/EC2/Example_AutoScaling/main.tf
+++ Resources/EC2/Example_AutoScaling/main.tf
@@ -2,60 +2,120 @@
 module "EC2_AutoScaling" {
   source = "../../../modules/EC2_AutoScaling"
   # launch_template
-  name_prefix = "test_template"
-  image_id = "ami-0b7546e839d7ace12"
-  instance_type = "t2.micro"
-  user_data = filebase64("userdata/example.sh")
-  vpc_security_group_ids = [module.SG.sg_id]
+  name_prefix            = "test_template"
+  image_id               = "ami-0b7546e839d7ace12"
+  instance_type          = "t2.micro"
+  user_data              = filebase64("userdata/example.sh")
+  vpc_security_group_ids = [module.sg_launch_template.sg_id]
   # autoscaling_group
   autoscaling_group_name = "test_autoscaling_group"
   # todo when add elb,change privatesubnet
   vpc_zone_identifier = [
     "subnet-0ef3a322300e969c2"
-    ]
+  ]
   desired_capacity = 1
-  max_size = 1
-  min_size = 1
+  max_size         = 1
+  min_size         = 1
+
+  # lb
+  alb_name           = "test-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups = [
+    module.sg_lb.sg_id
+  ]
+  subnets = [
+    "subnet-0ef3a322300e969c2",
+    "subnet-073f1baa14173c1a5"
+  ]
+
+  # target_group
+  target_group_name     = "test-target-group"
+  target_group_port     = 80
+  target_group_protocol = "HTTP"
+  target_group_vpc_id   = "vpc-05b5aeed5c9d8e83e"
+
+
 }
 
 # SecurityGroup
-module "SG" {
-  source = "../../../modules/SecurityGroup"
-  sg_name = "test_AS_security_group_01"
+module "sg_launch_template" {
+  source         = "../../../modules/SecurityGroup"
+  sg_name        = "test_AS_security_group_01"
   sg_description = "test_AS_security_group_01"
-  sg_vpc_id = "vpc-05b5aeed5c9d8e83e"
+  sg_vpc_id      = "vpc-05b5aeed5c9d8e83e"
 
   sg_rule = {
     "ingress_01" = {
-      type = "ingress"
-      to_port = 80
-      from_port = 80
-      protocol = "tcp"
+      type                     = "ingress"
+      to_port                  = 80
+      from_port                = 80
+      protocol                 = "tcp"
       source_security_group_id = null
-      cidr_blocks = ["0.0.0.0/0"]
-      description = "HTTP from Internet"
+      cidr_blocks              = ["0.0.0.0/0"]
+      description              = "HTTP from Internet"
     }
     "ingress_02" = {
-      type = "ingress"
-      to_port = 443
-      from_port = 443
-      protocol = "tcp"
+      type                     = "ingress"
+      to_port                  = 443
+      from_port                = 443
+      protocol                 = "tcp"
       source_security_group_id = null
-      cidr_blocks = ["0.0.0.0/0"]
-      description = "HTTPS from Internet"
+      cidr_blocks              = ["0.0.0.0/0"]
+      description              = "HTTPS from Internet"
     }
     "egress_01" = {
-      type = "egress"
-      to_port = 0
-      from_port = 0
-      protocol = "-1"
+      type                     = "egress"
+      to_port                  = 0
+      from_port                = 0
+      protocol                 = "-1"
       source_security_group_id = null
-      cidr_blocks = ["0.0.0.0/0"]
-      description = "Allow any outbound traffic"
+      cidr_blocks              = ["0.0.0.0/0"]
+      description              = "Allow any outbound traffic"
     }
   }
-  
 }
+
+module "sg_lb" {
+  source         = "../../../modules/SecurityGroup"
+  sg_name        = "test_lb_security_group_01"
+  sg_description = "test_lb_security_group_01"
+  sg_vpc_id      = "vpc-05b5aeed5c9d8e83e"
+
+  sg_rule = {
+    "ingress_01" = {
+      type                     = "ingress"
+      to_port                  = 80
+      from_port                = 80
+      protocol                 = "tcp"
+      source_security_group_id = null
+      cidr_blocks              = ["0.0.0.0/0"]
+      description              = "HTTP from Internet"
+    }
+    "ingress_02" = {
+      type                     = "ingress"
+      to_port                  = 443
+      from_port                = 443
+      protocol                 = "tcp"
+      source_security_group_id = null
+      cidr_blocks              = ["0.0.0.0/0"]
+      description              = "HTTPS from Internet"
+    }
+    "egress_01" = {
+      type                     = "egress"
+      to_port                  = 0
+      from_port                = 0
+      protocol                 = "-1"
+      source_security_group_id = null
+      cidr_blocks              = ["0.0.0.0/0"]
+      description              = "Allow any outbound traffic"
+    }
+  }
+}
+
+
+
+
 output "EC2_AutoScaling" {
   value = module.EC2_AutoScaling
 }
--- modules/EC2_AutoScaling/main.tf
+++ modules/EC2_AutoScaling/main.tf
@@ -1,19 +1,19 @@
 # launch_template
 resource "aws_launch_template" "launch_template" {
-  name_prefix   = var.name_prefix
-  image_id      = var.image_id
-  instance_type = var.instance_type
-  user_data = var.user_data
+  name_prefix            = var.name_prefix
+  image_id               = var.image_id
+  instance_type          = var.instance_type
+  user_data              = var.user_data
   vpc_security_group_ids = var.vpc_security_group_ids
 }
 
 # autoscaling_group
 resource "aws_autoscaling_group" "autoscaling_group" {
-  name = var.autoscaling_group_name
+  name                = var.autoscaling_group_name
   vpc_zone_identifier = var.vpc_zone_identifier
-  desired_capacity   = var.desired_capacity
-  max_size           = var.max_size
-  min_size           = var.min_size
+  desired_capacity    = var.desired_capacity
+  max_size            = var.max_size
+  min_size            = var.min_size
 
   launch_template {
     id      = aws_launch_template.launch_template.id
@@ -21,11 +21,54 @@ resource "aws_autoscaling_group" "autoscaling_group" {
   }
 }
 
+# lb
+resource "aws_lb" "lb" {
+  name               = var.alb_name
+  internal           = var.internal
+  load_balancer_type = var.load_balancer_type
+  security_groups    = var.security_groups
+  subnets            = var.subnets
+
+  enable_deletion_protection = var.enable_deletion_protection
+
+  # access_logs {
+  #   bucket  = aws_s3_bucket.lb_logs.bucket
+  #   prefix  = "test-lb"
+  #   enabled = true
+  # }
+
+  #   tags = {
+  #     Environment = "production"
+  #   }
+}
+
+# target_group
+resource "aws_lb_target_group" "tg" {
+  name        = var.target_group_name
+  port        = var.target_group_port
+  protocol    = var.target_group_protocol
+  vpc_id      = var.target_group_vpc_id
+  target_type = var.target_type
+  health_check {
+    interval            = var.health_check_interval
+    path                = var.health_check_path
+    port                = var.health_check_port
+    protocol            = var.health_check_protocol
+    timeout             = var.health_check_timeout
+    healthy_threshold   = var.health_check_healthy_threshold
+    unhealthy_threshold = var.health_check_unhealthy_threshold
+    matcher             = var.health_check_matcher
+  }
+}
 
 output "aws_launch_template_id" {
-    value = aws_launch_template.launch_template.id
+  value = aws_launch_template.launch_template.id
 }
 
 output "aws_autoscaling_group_id" {
-    value = aws_autoscaling_group.autoscaling_group.id
+  value = aws_autoscaling_group.autoscaling_group.id
+}
+
+output "aws_lb_id" {
+  value = aws_lb.lb.id
 }
