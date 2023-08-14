ef1e6a2bf4af8bf4b5dc7c2f4e7de66aaecca27c
Yuki Mitsuyama <55880586+y-mitsuyama1216@users.noreply.github.com>
2023-08-14 16:33:18 +0900

    wip

---
+++ Resources/ACM/outputs.tf
@@ -0,0 +1,7 @@
+output "acm_certificate_validation_certificate_arn_ap_northeast_1" {
+  value = module.acm
+}
+
+output "acm_certificate_validation_certificate_us_east_1" {
+  value = module.acm_use1
+}
