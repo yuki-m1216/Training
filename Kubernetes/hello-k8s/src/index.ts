e7247a594f1814bf84220d949a0508b0c634424c
Yuki Mitsuyama <19871216yuki.m@gmail.com>
2025-08-25 00:52:24 +0900

    wip

--- Kubernetes/hello-k8s/src/index.ts
+++ Kubernetes/hello-k8s/src/index.ts
@@ -1,12 +1,12 @@
 import express from 'express';
 
 const app = express();
-const port = 3000;
+const port = parseInt(process.env.PORT || '3000', 10);
 
 app.get('/', (req, res) => {
   res.send('Hello, Kubernetes!');
 });
 
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Server is running at http://0.0.0.0:${port}`);
 });
