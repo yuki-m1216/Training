8d96568923b9999ac641ee881917b5697f6f7a53
Yuki Mitsuyama <19871216yuki.m@gmail.com>
2025-09-10 22:33:29 +0900

    add frontend dockerfile

--- Kubernetes/learning-roadmap/project-02-chat-app/frontend/next.config.ts
+++ Kubernetes/learning-roadmap/project-02-chat-app/frontend/next.config.ts
@@ -2,6 +2,20 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+
+  output: "standalone",
+
+  env: {
+    NEXT_PUBLIC_SOCKET_URL: process.env.NEXT_PUBLIC_SOCKET_URL || "http://localhost:3000",
+  },
+
+  webpack: (config) => {
+    config.externals.push({
+      bufferutil:"bufferutil",
+      "utf-8-validate":"utf-8-validate"
+    });
+    return config;
+  }
 };
 
 export default nextConfig;
